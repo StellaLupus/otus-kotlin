3ff5035bc787ec4875ccb54e524bcd11113fd9d2
StellaLupus <soulofpain.k@gmail.com>
2023-07-04 17:34:24 +0300

    feat: 1st homework

---
+++ m1w1/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+    application
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
+}
